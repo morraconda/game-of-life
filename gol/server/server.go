e2f42d3917dbce635b08b8c044890700b60ab6ee
Dom <ze23128@bristol.ac.uk>
2024-11-10 16:23:28 +0000

    and again

--- gol/server/server.go
+++ gol/server/server.go
@@ -152,9 +152,9 @@ func main() {
 
 	//Start listener
 	go func() {
-		listener, err = net.Listen("tcp", *pAddr)
+		listener, err = net.Listen("tcp", ":8030")
 		if err != nil {
-			log.Fatalf("Failed to listen on %s: %v", *pAddr, err)
+			log.Fatalf("Failed to listen on :8030: %v", err)
 		}
 		defer listener.Close()
 		rpc.Accept(listener)
